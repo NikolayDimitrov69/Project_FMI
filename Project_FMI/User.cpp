fb093e7b7a62647e4fd828037507ecf2d9f6b968
NikolayDimitrov69 <4mi0800004@g.fmi.uni-sofia.bg>
2022-05-07 15:56:04 +0300

    Клас "User"

---
+++ Project_FMI/User.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "User.h"
+
+using std::string;
+
+void User::setUsername(string _username) {
+	username = _username;
+}
+string User::getName() {
+	return username;
+}
+
+void User::setPassword(string _password) {
+	password = _password;
+}
+string User::getPassword() {
+	return password;
+}
+
+void User::setEmail(string _email) {
+	email = _email;
+}
+string User::getEmail() {
+	return email;
+}
+
+User::User(string _username, string _password, string _email):username(_username),password(_password),email(_email) {
+
+}
---
+++ Project_FMI/User.h
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using std::string;
+
+class User {
+private:
+	string username;
+	string password;
+	string email;
+public:
+	//! Мутатор за потребителско име.
+	void setUsername(string);
+	//! Селектор за потребилтеско име.
+	string getName();
+	//! Мутатор за потребителска парола.
+	void setPassword(string);
+	//! Селектор за потребителска парола.
+	string getPassword();
+	//! Мутатор за потрелбилски имейл адрес.
+	void setEmail(string);
+	//! Слектор за потрелбилски имейл адрес.
+	string getEmail();
+
+	//! 
+	User(string username = "default", string password = "default", string email = "default");
+};
+
